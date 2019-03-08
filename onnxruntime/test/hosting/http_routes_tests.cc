f5118821498a7c9c139333ef0941857143abe2a7
tmccrmck <tjmcc01@gmail.com>
2019-03-08 09:58:52 -0800

    Adds more regex route tests

--- onnxruntime/test/hosting/http_routes_tests.cc
+++ onnxruntime/test/hosting/http_routes_tests.cc
@@ -35,6 +35,7 @@ TEST(PositiveTests, PostRouteTest) {
       std::make_tuple(http::verb::post, "/v1/models/abc/versions/23:predict", "abc", "23", "predict", http::status::ok),
       std::make_tuple(http::verb::post, "/v1/models/abc:predict", "abc", "", "predict", http::status::ok),
       std::make_tuple(http::verb::post, "/v1/models/models/versions/45:predict", "models", "45", "predict", http::status::ok),
+      std::make_tuple(http::verb::post, "/v1/models/??$$%%@@$^^/versions/45:predict", "??$$%%@@$^^", "45", "predict", http::status::ok),
       std::make_tuple(http::verb::post, "/v1/models/versions/versions/45:predict", "versions", "45", "predict", http::status::ok)};
 
   run_route(predict_regex, http::verb::post, actions, true);
@@ -44,21 +45,30 @@ TEST(NegativeTests, PostRouteInvalidURLTest) {
   auto predict_regex = R"(/v1/models/([^/:]+)(?:/versions/(\d+))?:(classify|regress|predict))";
 
   std::vector<test_data> actions{
+      std::make_tuple(http::verb::post, "", "", "", "", http::status::not_found),
       std::make_tuple(http::verb::post, "/v1/models", "", "", "", http::status::not_found),
       std::make_tuple(http::verb::post, "/v1/models:predict", "", "", "", http::status::not_found),
       std::make_tuple(http::verb::post, "/v1/models:bar", "", "", "", http::status::not_found),
       std::make_tuple(http::verb::post, "/v1/models/abc/versions", "", "", "", http::status::not_found),
       std::make_tuple(http::verb::post, "/v1/models/abc/versions:predict", "", "", "", http::status::not_found),
+      std::make_tuple(http::verb::post, "/v1/models/a:bc/versions:predict", "", "", "", http::status::not_found),
+      std::make_tuple(http::verb::post, "/v1/models/abc/versions/2.0:predict", "", "", "", http::status::not_found),
+      std::make_tuple(http::verb::post, "/models/abc/versions/2:predict", "", "", "", http::status::not_found),
       std::make_tuple(http::verb::post, "/v1/models/abc/versions/23:foo", "", "", "", http::status::not_found)};
 
   run_route(predict_regex, http::verb::post, actions, false);
 }
 
+// These tests are because we currently only support POST and GET
+// Some HTTP methods should be removed from test data if we support more (e.g. PUT)
 TEST(NegativeTests, PostRouteInvalidMethodTest) {
   auto predict_regex = R"(/v1/models/([^/:]+)(?:/versions/(\d+))?:(classify|regress|predict))";
 
   std::vector<test_data> actions{
-      std::make_tuple(http::verb::get, "/v1/models/abc/versions/23:predict", "abc", "23", "predict", http::status::method_not_allowed)};
+      std::make_tuple(http::verb::get, "/v1/models/abc/versions/23:predict", "abc", "23", "predict", http::status::method_not_allowed),
+      std::make_tuple(http::verb::put, "/v1/models", "", "", "", http::status::method_not_allowed),
+      std::make_tuple(http::verb::delete_, "/v1/models", "", "", "", http::status::method_not_allowed),
+      std::make_tuple(http::verb::head, "/v1/models", "", "", "", http::status::method_not_allowed)};
 
   run_route(predict_regex, http::verb::post, actions, false);
 }
