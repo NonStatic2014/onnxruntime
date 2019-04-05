9e5de8ad523ac2c22045456d7c903d8f8963bd70
Trent McCormick <tjmcc01@gmail.com>
2019-04-04 18:49:24 -0700

    Simplify executor_test to expose problems with logging_manager

--- onnxruntime/test/hosting/executor_test.cc
+++ onnxruntime/test/hosting/executor_test.cc
@@ -15,29 +15,12 @@ namespace test {
 
 TEST(PositiveTests, PredictInputRequestWorks) {
   auto model_file = "testdata/mul_1.pb";
+
   {
     onnxruntime::hosting::HostingEnvironment env(logging::Severity::kWARNING, logging::LoggingManager::InstanceType::Temporal);
     auto res = env.GetSession()->Load(model_file);
     EXPECT_TRUE(res.IsOK());
   }
-  /*auto res = env->GetSession()->Load("testdata/mul_1.pb");
-  if (!res.IsOK()) {
-    FAIL() << res.ErrorMessage();
-  }
-
-  onnxruntime::hosting::Executor executor(env, "RequestId");
-
-  onnxruntime::hosting::PredictRequest request{};
-  onnxruntime::hosting::PredictResponse response{};
-
-  const static std::string input_json = R"({"inputs":{"X":{"dims":[3,2],"dataType":1,"floatData":[1,2,3,4,5,6]}},"outputFilter":["Y"]})";
-  auto status = onnxruntime::hosting::GetRequestFromJson(input_json, request);
-
-  if (!status.ok()) {
-    FAIL() << status.ToString();
-  } */
-
-  // status = executor.Predict("sdf", "PSs", request, response);
 
   EXPECT_TRUE(true);
 }
