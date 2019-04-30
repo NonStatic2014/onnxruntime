0e3371cd7bd4c6cf7c766fb0dd1183b4c3f686fb
tmccrmck <tjmcc01@gmail.com>
2019-04-30 14:21:33 -0700

    Come up with minimal failing example

--- onnxruntime/test/server/unit_tests/executor_tests.cc
+++ onnxruntime/test/server/unit_tests/executor_tests.cc
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
+#include "test/framework/test_utils.h"
 #include "test/test_environment.h"
 #include "server/executor.h"
 #include "server/http/json_handling.h"
@@ -13,63 +14,12 @@ namespace onnxruntime {
 namespace server {
 namespace test {
 
-namespace {
-void CheckStringInFile(const std::string& filename, const std::string& look_for) {
-  std::ifstream ifs{filename};
-  std::string content(std::istreambuf_iterator<char>{ifs},
-                      std::istreambuf_iterator<char>{});
-
-  EXPECT_NE(content.find(look_for), std::string::npos);
-}
-
-void DeleteFile(const std::string& filename) {
-  int result = std::remove(filename.c_str());
-  EXPECT_EQ(result, 0);
-}
-}  // namespace
-
-TEST(PositiveTests, StolenFromSinksTest) {
-  const std::string filename{"TestCLogSink.out"};
-  const std::string logid{"CLogSink"};
-  const std::string message{"Test clog message"};
-  const logging::Severity min_log_level = logging::Severity::kWARNING;
-
-  // redirect clog to a file so we can check the output
-  std::ofstream ofs(filename);
-
-  auto old_rdbuf = std::clog.rdbuf();
-  std::clog.rdbuf(ofs.rdbuf());
-
-  // create scoped manager so sink gets destroyed once done
-  {
-    logging::LoggingManager manager{std::unique_ptr<logging::ISink>{new logging::CLogSink{}}, min_log_level, false,
-                                    logging::LoggingManager::InstanceType::Temporal};
-
-    auto logger = manager.CreateLogger(logid);
-
-    LOGS(*logger, WARNING) << message;
-  }
-
-  // check message was flushed to file before we close ofs.
-  CheckStringInFile(filename, message);
-
-  // revert redirection
-  std::clog.rdbuf(old_rdbuf);
-  ofs.close();
-
-  DeleteFile(filename);
-}
-
 TEST(PositiveTests, TestServerEnvironment) {
   const static std::string model_file = "testdata/mul_1.pb";
   const static std::string input_json = R"({"inputs":{"X":{"dims":[3,2],"dataType":1,"floatData":[1,2,3,4,5,6]}},"outputFilter":["Y"]})";
 
-  #ifdef HAVE_FRAMEWORK_LIB
-  std::cout << "@@@@@@@@@@@@@@@@@@@@@@" << std::endl;
-  #endif
-  // create scoped manager so sink gets destroyed once done
   {
-    onnxruntime::server::ServerEnvironment env(logging::Severity::kWARNING, logging::LoggingManager::InstanceType::Temporal, true);
+    /*onnxruntime::server::ServerEnvironment env(logging::Severity::kWARNING, logging::LoggingManager::InstanceType::Temporal, false);
     auto res = env.InitializeModel(model_file);
     EXPECT_TRUE(res.IsOK());
 
@@ -82,9 +32,15 @@ TEST(PositiveTests, TestServerEnvironment) {
     }
     const auto& logger = env.GetAppLogger();
 
-    LOGS(logger, WARNING) << "####################################&&&&&&&&&&&&&&&&&&&";
+    LOGS(logger, WARNING) << "####################################&&&&&&&&&&&&&&&&&&&";*/
     // status = executor.Predict("sdf", "PSs", request, response);
+    onnxruntime::SessionOptions options_{};
+    std::unique_ptr<onnxruntime::Environment> runtime_environment_;
+    //auto status = onnxruntime::Environment::Create(runtime_environment_);
+    //EXPECT_TRUE(status.IsOK());
+    onnxruntime::InferenceSession sess(options_, &::onnxruntime::test::DefaultLoggingManager());
   }
+  EXPECT_TRUE(true);
 }
 
 }  // namespace test
