1d7bb20bab625abb1f3ed8137fc4501de1d88db5
tmccrmck <tjmcc01@gmail.com>
2019-04-30 16:21:08 -0700

    Clean up executor_tests.cc

--- onnxruntime/test/server/unit_tests/executor_tests.cc
+++ onnxruntime/test/server/unit_tests/executor_tests.cc
@@ -15,32 +15,14 @@ namespace server {
 namespace test {
 
 TEST(PositiveTests, TestServerEnvironment) {
-  const static std::string model_file = "testdata/mul_1.pb";
-  const static std::string input_json = R"({"inputs":{"X":{"dims":[3,2],"dataType":1,"floatData":[1,2,3,4,5,6]}},"outputFilter":["Y"]})";
-
-  {
-    /*onnxruntime::server::ServerEnvironment env(logging::Severity::kWARNING, logging::LoggingManager::InstanceType::Temporal, false);
-    auto res = env.InitializeModel(model_file);
-    EXPECT_TRUE(res.IsOK());
-
-    onnxruntime::server::Executor executor(&env, "RequestId");
-    onnxruntime::server::PredictRequest request{};
-    onnxruntime::server::PredictResponse response{};
-    auto status = onnxruntime::server::GetRequestFromJson(input_json, request);
-    if (!status.ok()) {
-      FAIL() << status.ToString();
-    }
-    const auto& logger = env.GetAppLogger();
-
-    LOGS(logger, WARNING) << "####################################&&&&&&&&&&&&&&&&&&&";*/
-    // status = executor.Predict("sdf", "PSs", request, response);
-    onnxruntime::SessionOptions options_{};
-    std::unique_ptr<onnxruntime::Environment> runtime_environment_;
-    //auto status = onnxruntime::Environment::Create(runtime_environment_);
-    //EXPECT_TRUE(status.IsOK());
-    onnxruntime::InferenceSession sess(options_, &::onnxruntime::test::DefaultLoggingManager());
-  }
-  EXPECT_TRUE(true);
+  onnxruntime::SessionOptions options_{};
+  std::unique_ptr<onnxruntime::Environment> runtime_environment_;
+  #ifdef HAVE_FRAMEWORK_LIB
+    std::cout << "HAVE_FRAMEWORK_LIB enabled" << std::endl;
+  #else
+    auto status = onnxruntime::Environment::Create(runtime_environment_);
+  #endif
+  onnxruntime::InferenceSession sess(options_, &::onnxruntime::test::DefaultLoggingManager());
 }
 
 }  // namespace test
