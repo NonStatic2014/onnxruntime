70aa9dcf27e3928b87a1892d3218eab504ad19d5
tmccrmck <tjmcc01@gmail.com>
2019-05-03 16:09:24 -0700

    Finishes working executor_test for mul_1

--- onnxruntime/test/server/unit_tests/executor_tests.cc
+++ onnxruntime/test/server/unit_tests/executor_tests.cc
@@ -28,7 +28,7 @@ void DeleteFile(const std::string& filename) {
 }
 }  // namespace
 
-TEST(ExecutorTests, TestLogSink) {
+TEST(ExecutorTests, TestLogSinkWorks) {
   const std::string filename{"TestLogSink.out"};
   const std::string logid{"LogSink"};
   const std::string message{"Test log message"};
@@ -59,24 +59,31 @@ TEST(ExecutorTests, TestLogSink) {
 }
 
 TEST(ExecutorTests, TestMul_1) {
-  const static std::string model_file = "testdata/mul_1.pb";
-  const static std::string input_json = R"({"inputs":{"X":{"dims":[3,2],"dataType":1,"floatData":[1,2,3,4,5,6]}},"outputFilter":["Y"]})";
+  const static auto model_file = "testdata/mul_1.pb";
+  const static auto input_json = R"({"inputs":{"X":{"dims":[3,2],"dataType":1,"floatData":[1,2,3,4,5,6]}},"outputFilter":["Y"]})";
+  const static auto expected = R"({"outputs":{"Y":{"dims":["3","2"],"dataType":1,"floatData":[1,4,9,16,25,36]}}})";
 
   onnxruntime::server::ServerEnvironment env(logging::Severity::kWARNING, logging::LoggingManager::InstanceType::Temporal, false);
 
-  auto res = env.InitializeModel(model_file);
-  EXPECT_TRUE(res.IsOK());
+  auto status = env.InitializeModel(model_file);
+  EXPECT_TRUE(status.IsOK());
+
+  status = env.GetSession()->Initialize();
+  EXPECT_TRUE(status.IsOK());
 
   onnxruntime::server::Executor executor(&env, "RequestId");
   onnxruntime::server::PredictRequest request{};
   onnxruntime::server::PredictResponse response{};
 
-  auto status = onnxruntime::server::GetRequestFromJson(input_json, request);
-  if (!status.ok()) {
-    FAIL() << status.ToString();
-  }
-  executor.Predict()
+  auto protostatus = onnxruntime::server::GetRequestFromJson(input_json, request);
+  EXPECT_TRUE(protostatus.ok());
 
+  auto prediction_res = executor.Predict("Name", "version", request, response);
+  EXPECT_TRUE(prediction_res.ok());
+
+  std::string body;
+  protostatus = GenerateResponseInJson(response, body);
+  EXPECT_EQ(expected, body);
 }
 
 }  // namespace test
