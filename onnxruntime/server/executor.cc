3b74292cf1b379063ca0654ff668654269c807c8
tmccrmck <tjmcc01@gmail.com>
2019-05-04 17:21:27 -0700

    Use delete[] instead of free() for tensor data

--- onnxruntime/server/executor.cc
+++ onnxruntime/server/executor.cc
@@ -129,7 +129,7 @@ protobufutil::Status Executor::Predict(const std::string& model_name,
   // Manually free all memory in NameMLValMap
   for (auto& elem : name_ml_value_map) {
       void* buf = elem.second.GetMutable<Tensor>()->MutableDataRaw();
-      free(buf);
+      delete[] buf;
   }
 
   // Build the response
